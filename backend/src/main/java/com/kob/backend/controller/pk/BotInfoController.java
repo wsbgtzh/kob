ce6f2e37da5c5e19e10defe4947476ec2476167d
wsbgtzh <tangzuhan@gmail.com>
2023-05-28 17:06:05 +0800

    创建后端

---
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -0,0 +1,21 @@
+package com.kob.backend.controller.pk;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/pk/")
+public class BotInfoController {
+    @RequestMapping("getbotinfo/")
+    public Map<String, String> getbotInfo() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "tiger");
+        map.put("rating", "1500");
+        return map;
+    }
+}
